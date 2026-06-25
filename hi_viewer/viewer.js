15ac79491ad0c638a6ecf65c912169dec04a5e80
jedediahpidareese-coder <jedediahpidareese@gmail.com>
2026-06-24 19:21:38 -0500

    HI viewer: add the 10 page-images that existed but were never in the nav (incl. page 300 = Kanazawa officials pt 1), slotted next to each han's pages; +VER hi27. Two facing pages are neighbour domains (Tsurumaki, Nakatsu). First-pass annotation, adversarially verified.

--- hi_viewer/viewer.js
+++ hi_viewer/viewer.js
@@ -23,7 +23,7 @@ let currentLang = 'english_translation';
 
 const $ = (sel) => document.querySelector(sel);
 const xywhRe = /xywh=pixel:([0-9.]+),([0-9.]+),([0-9.]+),([0-9.]+)/;
-const VER = '20260626hi26';
+const VER = '20260626hi27';
 
 async function loadJson(path) {
   const r = await fetch(path);
