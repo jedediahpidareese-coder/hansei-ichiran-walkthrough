178ed9526a6456f067b0a3f5c008b5a5a445a63e
jedediahpidareese-coder <jedediahpidareese@gmail.com>
2026-09-23 00:37:44 -0500

    Viewer hi97: corrections from the 2026-09-22/23 re-read of the Hansei Ichiran scans (Numata, Oka, Kawagoe and Wakayama attributions; Sekiyado total; 10,678; 戍兵; 精兵; 二小分隊)

--- hi_viewer/viewer.js
+++ hi_viewer/viewer.js
@@ -23,7 +23,7 @@ let currentLang = 'english_translation';
 
 const $ = (sel) => document.querySelector(sel);
 const xywhRe = /xywh=pixel:([0-9.]+),([0-9.]+),([0-9.]+),([0-9.]+)/;
-const VER = '20260922hi96';
+const VER = '20260923hi97';
 
 async function loadJson(path) {
   const r = await fetch(path);
